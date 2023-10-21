69d91a53bbd9ba5b4a7bae2a3db141a116db88d3
rizqifebriansyah <rizqifebriansyah22@gmail.com>
2023-10-21 09:50:42 +0700

    evaluasi 30 menit

--- resources/views/dokter/formermdokter.blade.php
+++ resources/views/dokter/formermdokter.blade.php
@@ -17,7 +17,7 @@
                                     <center>
                                         <h3>Subject</h3>
                                     </center>
-                                    <textarea class="form-control" rows="4" placeholder=""></textarea>
+                                    <textarea class="form-control" id="subject" name="subject" rows="4" placeholder=""></textarea>
                                 </div>
                             </div>
                             <div class="col-sm-3">
@@ -26,7 +26,7 @@
                                     <center>
                                         <h3>Object</h3>
                                     </center>
-                                    <textarea class="form-control" rows="4" placeholder=""></textarea>
+                                    <textarea class="form-control" id="objek" name="objek" rows="4" placeholder=""></textarea>
                                 </div>
                             </div>
                             <div class="col-sm-3">
@@ -35,7 +35,7 @@
                                     <center>
                                         <h3>Assesmen</h3>
                                     </center>
-                                    <textarea class="form-control" rows="4" placeholder=""></textarea>
+                                    <textarea class="form-control" id="assesmen" name="assesmen" rows="4" placeholder=""></textarea>
                                 </div>
                             </div>
                             <div class="col-sm-3">
@@ -44,12 +44,33 @@
                                     <center>
                                         <h3>Predicate</h3>
                                     </center>
-                                    <textarea class="form-control" rows="4" placeholder=""></textarea>
+                                    <textarea class="form-control" id="preidcate" name="preidcate" rows="4" placeholder=""></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
+
+                                <div class="form-group">
+                                    <center>
+                                        <h3>Evaluasi 30 menit pertama</h3>
+                                    </center>
+                                    <textarea class="form-control" id="30p" name="30p" rows="4" placeholder=""></textarea>
+                                </div>
+                            </div>  <div class="col-sm-6">
+
+                                <div class="form-group">
+                                    <center>
+                                        <h3>Evaluasi 30 menit kedua</h3>
+                                    </center>
+                                    <textarea class="form-control" id="30k" name="30k" rows="4" placeholder=""></textarea>
                                 </div>
                             </div>
                         </div>
                     </tbody>
                 </table>
+
+                <!-- KPO -->
                 <div class="accordion" id="accordionExample8">
                     <div class="card">
                         <div class="card-header bg-success" id="headingOne">
@@ -281,7 +302,7 @@
                                         <div class="form-group">
                                             <label for="exampleFormControlSelect1">Diagnosa Pemeriksaan
                                                 Penunjang</label>
-                                            <input type="text" id="diagnosapemeriksaanpenunjang" class="form-control"
+                                            <input type="text" id="diagnosalabo" name="diagnosalabo" class="form-control"
                                                 value="">
                                         </div>
                                     </div>
@@ -295,7 +316,7 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <table id="tabeltindakan" class="table table-sm mt-3 table-hover">
+                                        <table id="tablelab" class="table table-sm mt-3 table-hover">
                                             <thead>
                                                 <th>Nama tindakan</th>
                                             </thead>
@@ -314,7 +335,7 @@
                                         <div class="card">
                                             <div class="card-header bg-secondary">Tindakan / Layanan Pasien</div>
                                             <div class="card-body">
-                                                <form action="" method="post" class="formtindakan">
+                                                <form action="" method="post" class="formlab">
                                                     <div class="input_fields_wrap">
                                                         <div>
                                                         </div>
@@ -356,7 +377,7 @@
                                         <div class="form-group">
                                             <label for="exampleFormControlSelect1">Diagnosa Pemeriksaan
                                                 Penunjang</label>
-                                            <input type="text" id="diagnosapemeriksaanpenunjang"
+                                            <input type="text" id="diagnosaradio" name="diagnosaradio"
                                                 class="form-control" value="">
                                         </div>
                                     </div>
@@ -371,7 +392,7 @@
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <table id="tabeltindakan1" class="table table-sm mt-3 table-hover">
+                                        <table id="tableradio" class="table table-sm mt-3 table-hover">
                                             <thead>
                                                 <th>Nama tindakan</th>
                                             </thead>
@@ -390,7 +411,7 @@
                                         <div class="card">
                                             <div class="card-header bg-secondary">Tindakan / Layanan Pasien</div>
                                             <div class="card-body">
-                                                <form action="" method="post" class="formtindakan1">
+                                                <form action="" method="post" class="formradio">
                                                     <div class="input_fields_wrap1">
                                                         <div>
                                                         </div>
@@ -597,7 +618,7 @@
 
 
     $(function() {
-        $("#tabeltindakan").DataTable({
+        $("#tablelab").DataTable({
             "responsive": false,
             "lengthChange": false,
             "pageLength": 5,
@@ -606,7 +627,7 @@
         });
     });
     $(function() {
-        $("#tabeltindakan1").DataTable({
+        $("#tableradio").DataTable({
             "responsive": false,
             "lengthChange": false,
             "pageLength": 5,
@@ -614,28 +635,28 @@
             "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
         });
     });
-    $('#tabeltindakan').on('click', '.pilihlayanan', function() {
+    $('#tablelab').on('click', '.pilihlayanan', function() {
         var max_fields = 10; //maximum input boxes allowed
         var wrapper = $(".input_fields_wrap"); //Fields wrapper
         var x = 1; //initlal text box count
-        kode = $(this).attr('kode')
-        namatindakan = $(this).attr('namatindakan')
-        tarif = $(this).attr('tarif')
-        id = $(this).attr('id')
-        jenis = $(this).attr('jenis')
+        kodelab = $(this).attr('kode')
+        namatindakanlab = $(this).attr('namatindakan')
+        tariflab = $(this).attr('tarif')
+        idlab = $(this).attr('id')
+        jenislab = $(this).attr('jenis')
 
 
         // e.preventDefault();
         if (x < max_fields) { //max input box allowed
             x++; //text box increment
             $(wrapper).append(
-                '<div class="form-row text-xs"><div class="form-group col-md-5"><label for="">Tindakan</label><input readonly type="" class="form-control form-control-sm" id="" name="namatindakan" value="' +
+                '<div class="form-row text-xs"><div class="form-group col-md-8"><label for="">Tindakan</label><input readonly type="" class="form-control form-control-sm" id="" name="namatindakan" value="' +
                 namatindakan +
                 '"><input hidden readonly type="" class="form-control form-control-sm" id="" name="kodelayanan" value="' +
                 kode +
                 '"><input hidden  readonly type="" class="form-control form-control-sm" id="" name="jenis" value="' +
                 jenis +
-                '"></div><div class="form-group col-md-1"><label for="inputPassword4">Jumlah</label><input type="" class="form-control form-control-sm" id="" name="qty" value="1"></div><div class="form-group col-md-1"><label for="inputPassword4">Disc</label><input type="" class="form-control form-control-sm" id="" name="disc" value="0"></div><div class="form-group col-md-1"><label for="inputPassword4">Cyto</label><input type="" class="form-control form-control-sm" id="" name="cyto" value="0"></div><i class="bi bi-x-square remove_field form-group col-md-2 text-danger"></i></div>'
+                '"></div><div class="form-group col-md-1"><label for="inputPassword4">Jumlah</label><input type="" class="form-control form-control-sm" id="" name="qty" value="1"></div><i class="bi bi-x-square remove_field form-group col-md-2 text-danger"></i></div>'
             );
             $(wrapper).on("click", ".remove_field", function(e) { //user click on remove
                 e.preventDefault();
@@ -645,28 +666,28 @@
         }
     });
 
-    $('#tabeltindakan1').on('click', '.pilihlayanan1', function() {
+    $('#tableradio').on('click', '.pilihlayanan1', function() {
         var max_fields = 10; //maximum input boxes allowed
         var wrapper = $(".input_fields_wrap1"); //Fields wrapper
         var x = 1; //initlal text box count
-        kode = $(this).attr('kode')
-        namatindakan = $(this).attr('namatindakan')
-        tarif = $(this).attr('tarif')
-        id = $(this).attr('id')
-        jenis = $(this).attr('jenis')
+        koderad = $(this).attr('kode')
+        namatindakanrad = $(this).attr('namatindakan')
+        tarifrad = $(this).attr('tarif')
+        idrad = $(this).attr('id')
+        jenisrad = $(this).attr('jenis')
 
 
         // e.preventDefault();
         if (x < max_fields) { //max input box allowed
             x++; //text box increment
             $(wrapper).append(
-                '<div class="form-row text-xs"><div class="form-group col-md-5"><label for="">Tindakan</label><input readonly type="" class="form-control form-control-sm" id="" name="namatindakan" value="' +
+                '<div class="form-row text-xs"><div class="form-group col-md-8"><label for="">Tindakan</label><input readonly type="" class="form-control form-control-sm" id="" name="namatindakan" value="' +
                 namatindakan +
                 '"><input hidden readonly type="" class="form-control form-control-sm" id="" name="kodelayanan" value="' +
                 kode +
                 '"><input hidden  readonly type="" class="form-control form-control-sm" id="" name="jenis" value="' +
                 jenis +
-                '"></div><div class="form-group col-md-1"><label for="inputPassword4">Jumlah</label><input type="" class="form-control form-control-sm" id="" name="qty" value="1"></div><div class="form-group col-md-1"><label for="inputPassword4">Disc</label><input type="" class="form-control form-control-sm" id="" name="disc" value="0"></div><div class="form-group col-md-1"><label for="inputPassword4">Cyto</label><input type="" class="form-control form-control-sm" id="" name="cyto" value="0"></div><i class="bi bi-x-square remove_field form-group col-md-2 text-danger"></i></div>'
+                '"></div><div class="form-group col-md-1"><label for="inputPassword4">Jumlah</label><input type="" class="form-control form-control-sm" id="" name="qty" value="1"></div><i class="bi bi-x-square remove_field form-group col-md-2 text-danger"></i></div>'
             );
             $(wrapper).on("click", ".remove_field", function(e) { //user click on remove
                 e.preventDefault();
