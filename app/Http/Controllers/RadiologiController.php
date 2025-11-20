f5bfceed9667261fd7ae3338f6b830b288ec0be4
rizqifebriansyah <rizqifebriansyah22@gmail.com>
2025-11-20 14:25:54 +0700

    updatee

--- app/Http/Controllers/RadiologiController.php
+++ app/Http/Controllers/RadiologiController.php
@@ -621,7 +621,7 @@ class RadiologiController extends Controller
                     'ADMITDATE' => $tglmasuk,
                     'EFFECTIVEDATE' => $tglmasuk,
 
-                    'RELEVANTCLINICALINFO' => $request->diagnosa.nl2br($alamat),
+                    'RELEVANTCLINICALINFO' => $request->diagnosa.nl2br("\n\n\n\n|||").$alamat,
                     // 'PROCEDUREID' => $arr['kodelayanan'],
                     'PROCEDURE' => $arr['kodelayanan'],
                     'SPECIFIEDRADIOLOGISTID' => $request->dorad,
